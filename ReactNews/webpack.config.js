41ee1d20b740d8a1a3b3c033a88176985a3ab646
银白色暴风雪 <a543055979@qq.com>
2017-03-11 13:54:55 +0800

    项目初始化

--- ReactNews/webpack.config.js
+++ ReactNews/webpack.config.js
@@ -1,20 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    context: __dirname + "/src",
-    entry: "./js/index.js",
+    context: path.join(__dirname),
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./src/js/root.js",
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            query: {presets: ['react', 'es2015']}
-        }]
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins: ['react-html-attrs'], //添加组件的插件配置
+                }
+            },
+            //下面是使用 ant-design 的配置文件
+            { test: /\.css$/, loader: 'style-loader!css-loader' }
+        ]
     },
     output: {
-        path: __dirname + "/src/",
+        path: __dirname,
         publicPath: "/src/",
-        filename: "bundle.js"
-    }
+        filename: "./src/bundle.js"
+    },
+    plugins: debug ? [] : [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+        ],
 };
